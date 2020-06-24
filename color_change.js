9813f2c74beca67e64c9cbcef20ba4fcba87969a
webmemory <65940109+webmemory@users.noreply.github.com>
2020-06-24 16:39:06 +0900

    페이지 상단부분 수정

--- color_change.js
+++ color_change.js
@@ -1,18 +1,23 @@
 var setcolor={
   ofLinks:function(color){
-    var list_b=document.querySelectorAll('a');
-    var i=0;
-    while(i<list_b.length){
-      list_b[i].style.color=color;
-      i=i+1}
+    // var list_b=document.querySelectorAll('a');
+    // var i=0;
+    // while(i<list_b.length){
+    //   list_b[i].style.color=color;
+    //   i=i+1}
+    $('a').css('color',color)
     },
 
   ofBackground:function(color){
     document.querySelector('body').style.backgroundColor=color
+    // $('body').css('color',color)
+    // body 에 jquery 사용하면 오류가 발생함.
   },
 
   ofText:function(color){
     document.querySelector('body').style.color=color
+    // $('body').css('color',color)
+        // body 에 jquery 사용하면 오류가 발생함.
   }
 };
 
